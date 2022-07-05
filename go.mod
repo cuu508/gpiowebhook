a5f4fa971256289e6c96cb756d22d48966bcc598
Pēteris Caune <cuu508@gmail.com>
2022-07-05 14:35:08 +0300

    Initial commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/cuu508/gpiowebhook
+
+go 1.18
+
+require github.com/stianeikeland/go-rpio/v4 v4.6.0
---
+++ gpiowebhook.go
@@ -0,0 +1,40 @@
+package main
+
+import "github.com/stianeikeland/go-rpio/v4"
+import "log"
+import "io"
+import "net/http"
+import "os"
+import "time"
+
+func main() {
+    KEY := os.Getenv("KEY")
+
+    err := rpio.Open()
+    if err != nil {
+        log.Fatalln("Could not initialize rpio: ", err)
+    }
+
+    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+        key := r.URL.Query().Get("key")
+        if (key != KEY) {
+            log.Printf("Bad key: %s", key)
+            http.Error(w, "", 403)
+            return
+        }
+
+        go func() {
+            log.Println("Triggering pin 17")
+            pin := rpio.Pin(17)
+            pin.Output()
+            pin.High()
+            time.Sleep(time.Millisecond * 100)
+            pin.Low()
+        }()
+
+        io.WriteString(w, "OK!")
+    })
+
+    log.Println("Listening on port 28384...")
+    log.Fatal(http.ListenAndServe(":28384", nil))
+}
